10040a7ecd62f014b698fab6927eaa41ef8fb119
DANIELVELEV <s195059@uos.ac.uk>
2019-01-10 18:30:03 +0200

    flipping the character's facing point

--- Assets/Scripts/Main_Character.cs
+++ Assets/Scripts/Main_Character.cs
@@ -7,21 +7,35 @@ public class Main_Character : MonoBehaviour
     private Rigidbody2D myRigidbody;
     [SerializeField] // this prevents other scripts from changing the speed of the main character
     private float movementSpeed;
-    // Start is called before the first frame update
+
+    private bool facingLeft;
+
     void Start()
     {
+        facingLeft = true;
         myRigidbody = GetComponent<Rigidbody2D>(); // this is the reference to the main character's rigidbody
     }
 
-    // Update is called once per frame
     void FixedUpdate() //keeps the game fps on the same level for all PC's
     {
         float horizontal = Input.GetAxis("Horizontal");  //this function gives the player control over the main character on the x axis 
-        HandleMovement(horizontal); 
+        HandleMovement(horizontal);
+
+        Flip(horizontal);
     }
     private void HandleMovement(float horizontal) // this function handles all of the movement for the main character in the game
     {
         myRigidbody.velocity = new Vector2(horizontal * movementSpeed, myRigidbody.velocity.y); //this function is used  to apply force to the rigidbody of the main character, moving him across horizontal axis
     }
+    private void Flip(float horizontal) // this function helps to flip the facing position of the character
+    {
+        if (horizontal < 0 && !facingLeft || horizontal > 0 && facingLeft)
+        {
+            facingLeft = !facingLeft;
 
+            Vector3 theScale = transform.localScale; // this code line references the local scale of the main character in unity, so that he can be flipped 
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+    }
 }
